fc684a57ef2579fb62af0cc3c716c122bc1def3f
Brian Holdefehr <bhold45@gmail.com>
2020-03-29 21:38:48 -0400

    add ability to make fips the outer key

--- create_covid_json.py
+++ create_covid_json.py
@@ -6,8 +6,8 @@ import json
 from statistics import mean
 
 
-def read_covid_file(
-        filename, date_data, moving_average_days, is_state_file=False):
+def read_covid_file(filename, output_data, moving_average_days,
+        output_fips_first, is_state_file=False):
     """
     For a supplied csv file, containing either state or county covid data,
     this function will output a date-keyed dict in the format:
@@ -19,6 +19,18 @@ def read_covid_file(
       Example:
       {"2020-03-27":
          {"56043": {"cases": 12, "moving_average": 0.19}, ...}
+       ...
+      }
+
+    Alternatively, when output_fips_first=True, the format will be:
+      {"FIPS_ID":
+         {"YYYY-MM-DD": {"cases": X, "moving_average": X.X}, ...}
+       ...
+      }
+
+      Example:
+      {"56043":
+         {"2020-03-27": {"cases": 12, "moving_average": 0.19}, ...}
        ...
       }
     """
@@ -39,34 +51,52 @@ def read_covid_file(
         # This will store case count for each day chronologically moving
         # backwards from the latest date in the dataset.
         total_days_of_data = (latest_date - earliest_date).days + 1
-        fips_list_data = defaultdict(lambda: [None]*total_days_of_data)
+        inverse_chronological_case_data = \
+            defaultdict(lambda: [None]*total_days_of_data)
 
         # Skip the initial header line.
         for row in csv_list[1:]:
-            # date_data is keyed by date, and then FIPS.
-            # {"2020-03-27":
-            #    {"56043": {"cases": 12}, ...}
-            # }
-            date_data[row[DATE]][row[FIPS]] = {"cases": int(row[CASES] or 0)}
+            if output_fips_first:
+                # output_data is keyed by date, and then FIPS.
+                # {"56043":
+                #    {"2020-03-27": {"cases": 12}, ...}
+                # }
+                output_data[row[FIPS]][row[DATE]] = \
+                    {"cases": int(row[CASES] or 0)}
+            else:
+                # output_data is keyed by date, and then FIPS.
+                # {"2020-03-27":
+                #    {"56043": {"cases": 12}, ...}
+                # }
+                output_data[row[DATE]][row[FIPS]] = \
+                    {"cases": int(row[CASES] or 0)}
 
             # Store the number of cases in the fips list at the position which
             # represents the number of days since the latest (current) date.
             this_date = datetime.strptime(row[0], '%Y-%m-%d').date()
             day_offset = (latest_date - this_date).days
-            fips_row = fips_list_data[row[FIPS]]
+            fips_row = inverse_chronological_case_data[row[FIPS]]
             fips_row[day_offset] = int(row[CASES] or 0)
 
     # Add the daily moving average data.
     # This is the percent daily moving average in new cases over the prior
     # moving_average_days days.
-    for date_string, fips_entries in date_data.items():
-        for fips_id, cases_data in fips_entries.items():
-            # Find the slice of fips_list_data cooresponding to the preceding
-            # moving_average_days days for this fips location.
+    for outer_key, entries in output_data.items():
+        for inner_key, cases_data in entries.items():
+            if output_fips_first:
+                fips_id = outer_key
+                date_string = inner_key
+            else:
+                fips_id = inner_key
+                date_string = outer_key
+
+            # Find the slice of inverse_chronological_case_data cooresponding
+            # to the preceding moving_average_days days for this fips location.
             this_date = datetime.strptime(date_string, '%Y-%m-%d').date()
             start = (latest_date - this_date).days
             end = min(start+moving_average_days, total_days_of_data)
-            preceding_case_counts = fips_list_data[fips_id][start:end]
+            preceding_case_counts = \
+                inverse_chronological_case_data[fips_id][start:end]
             if len(preceding_case_counts) != moving_average_days or \
                     None in preceding_case_counts:
                 # We don't have enough days of data, or have faulty data.
@@ -79,19 +109,25 @@ def read_covid_file(
                 increase_percents.append((preceding_case_counts[i] -
                     preceding_case_counts[i + 1]) /
                         preceding_case_counts[i + 1])
-            date_data[date_string][fips_id]['moving_avg'] = mean(
-                increase_percents)
 
-    return date_data
+            if output_fips_first:
+                output_data[fips_id][date_string]['moving_avg'] = mean(
+                    increase_percents)
+            else:
+                output_data[date_string][fips_id]['moving_avg'] = mean(
+                    increase_percents)
+
+    return output_data
 
 
-def generate_json(
-        counties_file, states_file, output_file, moving_average_days):
+def generate_json(counties_file, states_file, output_file, moving_average_days,
+        output_fips_first):
     empty_data = defaultdict(dict)
     state_data = read_covid_file(
-        states_file, empty_data, moving_average_days, is_state_file=True)
+        states_file, empty_data, moving_average_days, output_fips_first,
+        is_state_file=True)
     state_and_county_data = read_covid_file(
-        counties_file, state_data, moving_average_days)
+        counties_file, state_data, moving_average_days, output_fips_first)
 
     with open(output_file, "w") as output:
         json.dump(state_and_county_data, output)
@@ -107,7 +143,10 @@ parser.add_argument("--output_file", help="File path to output JSON file.",
 parser.add_argument("--moving_average_days",
     help="The number of preceding days' case number changes to average.",
     default=5, type=int)
+parser.add_argument("--output_fips_first",
+    help="Output JSON will be top-level keyed by FIPS if True. Date if False.",
+    default=False, type=bool)
 args = parser.parse_args()
 
 generate_json(args.county_input, args.state_input, args.output_file,
-    args.moving_average_days)
+    args.moving_average_days, args.output_fips_first)
