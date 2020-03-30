7956440b5397b65b240cda4db72ad048e50b903e
Matthew Parrilla <matthew.parrilla@gmail.com>
2020-03-30 17:01:25 -0400

    Resolve merge conflict

--- src/js/dataTable.js
+++ src/js/dataTable.js
@@ -1,5 +1,8 @@
 import { fetchData, filterOutCounties, sortDateString } from './utilities';
 
+// Used to store our table data for sorting, etc
+let tableData;
+
 function sortRowsByColumn(sortColumn) {
   const intColumns = ["cases"];
   const floatColumns = ["cases_per_capita", "moving_avg"];
@@ -18,20 +21,22 @@ function sortRowsByColumn(sortColumn) {
 }
 
 // Put data in table and sort. Order: State, Cases, Cases Per Capita, Growth Rate
-function updateTable(rows, sortColumn = "state", descendingSort = true) {
+function updateTable(sortColumn = "state", descendingSort = true) {
   // sort rows in place
-  rows.sort(sortRowsByColumn(sortColumn));
+  tableData.sort(sortRowsByColumn(sortColumn));
 
   // TODO: to improve performance, could do this in sortRowsByColumn
   if (!descendingSort) {
-    rows.reverse();
+    tableData.reverse();
   }
 
   // Only show county column header if we are looking at county level data
-  document.getElementById("county_header").style.display = rows[0].county ? "table-cell" : "none";
+  document.getElementById("county_header").style.display = tableData[0].county ? "table-cell" : "none";
+
+  // TODO: only show state if we are looking at whole country
 
   const tbody = document.getElementById("js_tbody");
-  tbody.innerHTML = rows.map(row => `
+  tbody.innerHTML = tableData.map(row => `
     <tr>
       ${row.county ? `<td>${row.county}</td>` : ""}
       <td>${row.state}</td>
@@ -46,19 +51,12 @@ function updateTable(rows, sortColumn = "state", descendingSort = true) {
     </tr>`).join('');
 }
 
-export default async function initDataTable() {
+export default async function initDataTable(fips) {
   const [fipsData, casesByDate] = await fetchData();
   const dates = sortDateString(casesByDate);
   const today = casesByDate[dates[dates.length - 1]];
   const stateData = filterOutCounties(fipsData);
 
-  // Merge FIPS data with case data and calculate
-  const mergedStateData = stateData.map(stateFips => ({
-    ...stateFips,
-    ...today[stateFips.fips],
-    // using underscores to match style of source data
-    cases_per_capita: today[stateFips.fips].cases / stateFips.population
-  }));
 
   // Add handlers to sort on column header click
   const tableHeaders = document.querySelectorAll("#js_thead th");
@@ -73,10 +71,20 @@ export default async function initDataTable() {
 
       descendingSort = !descendingSort;
       th.classList.add(descendingSort ? "descending" : "ascending");
-      updateTable(mergedStateData, th.dataset.column, descendingSort);
+      updateTable(th.dataset.column, descendingSort);
     });
   });
 
+  if (fips == null) {
+    // Merge FIPS data with case data and calculate
+    tableData = stateData.map(stateFips => ({
+      ...stateFips,
+      ...today[stateFips.fips],
+      // using underscores to match style of source data
+      cases_per_capita: today[stateFips.fips].cases / stateFips.population
+    }));
+  }
+
   // init our table
-  updateTable(mergedStateData, "cases");
+  updateTable("cases");
 }
